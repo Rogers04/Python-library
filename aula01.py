094c824afe2ac9cfb7561ad717bc110988f599dd
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2025-12-09 21:24:40 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,11 @@
+# É assim que se escreve comentário
+"""
+Se chama: DocString
+
+Isso não é um comentário, mas você pode usar para fazer anotações maiores nos
+códigos. Uma explicação detalhada ou algo do genêro
+
+"""
+
+
+print(123)
---
+++ aula02.py
@@ -0,0 +1,7 @@
+# Esse argumento sep significa separador
+
+print(12, 34, 42, sep="-")
+print(56, 78, sep='-')
+
+
+
