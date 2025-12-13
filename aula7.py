71a34f065bfe6060c431d26b28168815bbbc3dac
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2025-12-12 21:16:20 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,23 @@
+# Variáveis são usadas para salvar algo na memória do computador
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+
+
+
+# nome_completo = 'Rogers Lucas Damaceno'
+# soma_numeros = 2 + 2
+# int_um = int('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_numeros) # Rogers Lucas Damaceno
+
+
+nome = 'Rogers'
+idade = 18
+maior_de_idade = idade >= 18
+
+
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
