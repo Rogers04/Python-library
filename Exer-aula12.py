ba8b2c646d9a9180ef71b1bba356415fcf343d3b
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2025-12-15 21:08:33 -0300

    Add files via upload

---
+++ Exer-aula12.py
@@ -0,0 +1,6 @@
+nome = 'Rogers Lucas Damaceno'
+altura = 1.70
+peso = 60
+imc = peso / (altura*altura)
+
+print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos, e seu IMC é:', imc)
---
+++ aula11.py
@@ -0,0 +1,7 @@
+# 1. (n + n)
+# 2. **
+# 3. * / // %
+# 4. + -
+
+conta_1 = (1 + 1) ** (5 + 5) # O python sempre irá ler primeiro os números dentro dos parentes
+print(conta_1)               # Depois ele irá ler o resto;
