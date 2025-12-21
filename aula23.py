87d407d5c09f5d550a1f2c6f2af4278401194111
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2025-12-20 22:54:01 -0300

    Add files via upload

---
+++ aula23.py
@@ -0,0 +1,30 @@
+'''
+Operador lógico "not"
+Usando para inverter expressões
+not True = False
+not False = True
+
+
+'''
+
+
+''''senha = input('Senha: ')
+
+
+if senha != '123456':
+    print('Senha incorreta.')'''
+
+
+'''if senha == '123456':
+    print('Você está dentro do sistema')
+
+
+else:
+    print('Senha inválida.')'''
+
+
+
+
+print(not True) # False
+print(not False) # True
+
