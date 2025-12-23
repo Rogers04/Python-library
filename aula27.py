c2bc2d56ba197f6bf5f14753bed97b0453ef57ba
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2025-12-23 20:56:48 -0300

    Add files via upload

---
+++ aula27.py
@@ -0,0 +1,15 @@
+'''
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd de caracteres da str
+'''
+
+
+variavel = 'Olá mundo'
+print(variavel[4:8]) # print = mund
+print(len(variavel)) # print = 9  "Ele contou quantas letras tem"
+print(variavel[:: -1]) # print = odnum álO
+
