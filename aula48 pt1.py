00b9a25635698455c3e7b7c5261729978918b157
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-08 21:59:11 -0300

    Add files via upload

---
+++ aula48 pt1.py
@@ -0,0 +1,19 @@
+'''
+Lists in python
+Type list - Mutable
+Support multiple value of any type
+Knowledge reusable - indexes and slicing
+useful methods: append, insert, pop, del, clear, extend, + 
+'''
+
+#         01234
+#        -54321
+string = 'ABCDE' # 5 characters (len)
+# print (bool([])) # falsy
+# print (lista, type(lista))
+
+#        0     1        2            3    4
+#       -5    -4       -3           -2   -1
+lista = [123, True, 'Rogers Lucas', 1.2, []] 
+print(lista)
+print(lista[2], type(lista[2]))
