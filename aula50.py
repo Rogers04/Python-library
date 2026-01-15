546fdca9e9e14ab41f8723d8a3dfdf2b13903247
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-14 22:50:52 -0300

    Add files via upload

---
+++ aula50.py
@@ -0,0 +1,12 @@
+# Create a counter of things you have on your list
+
+# My solution
+
+lista = ['Rogers', 'Lucas', 'Damaceno']
+lista.append('Maria')
+
+i = -1
+
+for nome in lista:
+    i += 1
+    print(i, nome)
