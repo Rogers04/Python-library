8a9d68069a3666aceddfeec8b4653fd524fd2b14
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-14 22:51:08 -0300

    Add files via upload

---
+++ aula50 continuetion.py
@@ -0,0 +1,11 @@
+# Create a counter of things you have on your list
+
+# My solution
+
+lista = ['Rogers', 'Lucas', 'Damaceno']
+lista.append('Maria')
+
+
+for item in enumerate(lista):
+    i, n = item
+    print(i, n)
