27cdb5a2e33728402d3d8a6ccc1f7b0e0ef07f6e
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-15 22:47:29 -0300

    Add files via upload

---
+++ aula52.py
@@ -0,0 +1,7 @@
+'''
+Type tupla - only list immutable
+'''
+name = ['Maria', 'Helena', 'Luiz']
+name = tuple(name)      # if you want to change a list for a tuple you do this
+print(name[0])
+print(name)
