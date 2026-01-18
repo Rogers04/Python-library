e87aba3a4a9bb26fd9a10ce21c51532f16f54980
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-17 22:03:01 -0300

    Add files via upload

---
+++ aula57.py
@@ -0,0 +1,24 @@
+'''
+list of lists and their index
+'''
+
+salas = [ 
+      # 0       1
+    [ 'Maria', 'Helena', ],   # 0
+
+      # 0
+    ['Elaine', ], # 1
+      
+      #0       1       2
+    ['Luiz', 'João', 'Eduarda',]#(0, 10, 20, 30, 40)],   # 2 
+]
+'''
+print(salas[1][0])
+print(salas[0][1])
+print(salas[2][2])
+print(salas[2][3][2])
+'''
+
+for sala in salas:
+    for aluno in sala:
+        print(aluno)
