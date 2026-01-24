749bebad3bd239652d361f89c5b7e655735a17df
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-23 21:12:29 -0300

    Add files via upload

---
+++ aula60.py
@@ -0,0 +1,14 @@
+'''
+Operator ternaty (condition of one line)
+<value> if <condition> else <other value>
+'''
+'''
+condition = 10 == 11
+variable = 'Value' if condition else 'Other value'
+
+print(variable)
+'''
+
+enter = 9
+new_enter = enter if enter <= 9 else 0
+print(new_enter)
