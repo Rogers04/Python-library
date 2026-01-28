dbcd38f9fd1fafd82dd06efe0ad43cbb95fee8f2
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-01-27 22:26:23 -0300

    Add files via upload

---
+++ aula65.py
@@ -0,0 +1,31 @@
+'''
+Introduction to functions (def) in Python
+Functions are pieces of code used to
+replicate a certain action throughout your code.
+They can receive values for parameters (arguments)
+and return a specific value.
+By default, Python functions return None
+'''
+
+'''def Print():
+    print('Várias1')
+    print('Várias2')
+    print('Várias3')
+    print('Várias4')
+
+Print()'''
+
+'''def imprimir(a, b, c):
+    print(a, b, c)
+
+imprimir(1, 2, 3)
+imprimir(4, 5, 6)
+'''
+
+def saudation(name):
+    print(f'Hello {name}')
+
+saudation('Rogers')
+saudation('Lucas')
+saudation('Damaceno')
+saudation()
