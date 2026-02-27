5a9160d03c75affc6fb68e4468bb907dc1df3a51
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2026-02-26 21:33:29 -0300

    Add files via upload

---
+++ aula.78 part2.py
@@ -0,0 +1,45 @@
+# Sets - Sets in Python (set type)
+# Sets are taught in mathematics
+# Sets in Python are mutable, but accept only
+# immutable types as internal values.
+
+
+# Creating a set
+# set(iterable) or {1, 2, 3}
+
+# s1 = set('Rogers')
+# s2 = {'Rogers', 1, 2, 3}
+# print(s1)
+# print(s2)
+
+# s1 = set()  # Empty
+# s1 = {'Rogers', 1, 2, 3}    # with data
+
+# Sets are efficient for removing duplicate values
+# from iterables.
+# - They do not accept mutable values;
+# - Their values ​​will always be unique;
+# - They do not have indexes;
+# - They do not guarantee order;
+# - They are iterable (for, in, not in)
+
+# li = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(li)
+# l2 = list(s1)
+# print(l2)
+
+s1 = {1, 2, 3}
+print(4 not in s1)
+
+
+#for number in s1:
+ #   print(number)
+
+# Useful methods:
+# add, update, clear, discard
+
+# Useful operators:
+# union | union - Joins
+# intersection & - Items present in both
+# difference - Items present only in the left set
+# symmetric difference ^ - Items not in both
